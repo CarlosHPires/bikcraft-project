dcce44da465a1019deb5cf194bc89250636fa427
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2022-10-12 01:25:16 -0300

    fix: I switched from pt-br to en some functions

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@ const links = document.querySelectorAll(".header-menu a");
 
 console.log("javascript".includes("teste"));
 
-function ativarLink(link) {
+function linkActivate(link) {
   const url = location.href;
   const href = link.href;
 
   if (url.includes(href)) {
-    link.classList.add("ativo");
+    link.classList.add("active");
   }
 }
 
-links.forEach(ativarLink);
+links.forEach(linkActivate);
 
 // Ativar itens do orçamento
 
@@ -26,3 +26,43 @@ function productAtivate(param) {
 }
 
 params.forEach(productAtivate);
+
+// Perguntas frequentes
+
+const questions = document.querySelectorAll(".questions button");
+
+function questionActivate(event) {
+  const question = event.currentTarget;
+  const controls = question.getAttribute("aria-controls");
+  const answer = document.getElementById(controls);
+
+  answer.classList.toggle("activate");
+  const activate = answer.classList.contains("activate");
+  question.setAttribute("aria-expanded", activate);
+}
+
+function questionsEvents(question) {
+  question.addEventListener("click", questionActivate);
+}
+
+questions.forEach(questionsEvents);
+console.log(questions);
+
+// Galeria Imagens
+
+const gallery = document.querySelectorAll(".bicycle-images img");
+const containerGallery = document.querySelector(".bicycle-images");
+
+function changeImage(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    containerGallery.prepend(img);
+  }
+}
+
+function eventsGallery(img) {
+  img.addEventListener("click", changeImage);
+}
+
+gallery.forEach(eventsGallery);
